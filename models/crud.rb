7e001d283cc808c668e6ce30c2f349d0c9a2218e
James Cox <j-cox03@hotmail.com>
2017-06-09 20:10:50 +0100

    made and tested extendable class Crud for all classes that use crud actions

--- models/crud.rb
+++ models/crud.rb
@@ -4,10 +4,41 @@ class Crud
 
   attr_reader :id
 
-  def initialize(options)
+  protected
+
+  def initialize(options, table_name)
     @id = options["id"] if options["id"]
+    @table_name = table_name
   end
 
+  def save(columns, values)
+    columns_string = build_column_string(columns)
+    sql_command = "INSERT INTO #{@table_name}
+      (#{columns_string}) VALUES ($1, $2) RETURNING id"
+    @id = SqlRunner.run(sql_command, values)[0]["id"]
+  end
 
+  def update(columns, values)
+    columns_string = build_column_string(columns)
+    sql_command = "UPDATE customers SET
+      (#{columns_string}) = ($1, $2)
+      WHERE id = #{@id}"
+    SqlRunner.run(sql_command, values)
+  end
+
+  def delete()
+    sql_command = "DELETE FROM #{@table_name} WHERE id = #{@id}"
+    SqlRunner.run(sql_command)
+  end
+
+  private
+
+  def build_column_string(columns)
+    result = ""
+    columns.each() do |column|
+      result += "#{column}, "
+    end
+    return result[0..-3]
+  end
 
 end
