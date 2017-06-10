c0e1ad7b96ffed016c5a74df97d95854daaaf375
James Cox <j-cox03@hotmail.com>
2017-06-10 11:45:07 +0100

    Made and tests methods get_tickets and get_customers in class Film. unsure if get_customers is required

--- models/film.rb
+++ models/film.rb
@@ -50,4 +50,30 @@ class Film < Crud
     return Screening.map_sql_result(screenings)
   end
 
+  def get_tickets()
+    sql_command = "SELECT tickets.* FROM films
+      INNER JOIN screenings
+      ON films.id = screenings.film_id
+      INNER JOIN tickets
+      ON screenings.id = tickets.screening_id
+      WHERE films.id = #{@id}
+      ORDER BY tickets.screening_id"
+    tickets = SqlRunner.run(sql_command)
+    return Ticket.map_sql_result(tickets)
+  end
+
+  # Is this really needed as a similar method will be available is class
+  # Screening and customers will be retrevable from ticket objects.
+  # def get_customers()
+  #   sql_command = "SELECT customers.* FROM films
+  #     INNER JOIN screenings
+  #     ON films.id = screenings.film_id
+  #     INNER JOIN tickets
+  #     ON screenings.id = tickets.screening_id
+  #     INNER JOIN customers
+  #     ON tickets.customer_id = customers.id
+  #     WHERE films.id = #{@id}"
+  #   customers = SqlRunner.run(sql_command)
+  #   return Customer.map_sql_result(customers)
+  # end
 end
